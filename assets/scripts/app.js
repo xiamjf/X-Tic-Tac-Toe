a6820c036172c2f1857e070df22ce7d047e2e746
xiamjf <xiamjf@gmail.com>
2018-11-04 12:03:45 -0500

    Added game logic & functionality to JS + CSS
    Now the game tracks each player and their move
    Player 1 plots X's by changing the color of the box they pick,
    player 2 plots O's in the same way

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,8 +11,34 @@
 
 $(() => {
   // your JS code goes here
+
+  // Okurrrrr. So, this variable will allow for either an X or an O to pop up depending on the player
+  // either player 1 or player 2
+  // whether the X or O shows up will be determined by the class that this variable
+  // indicates to the square
+  let player = 1
   // making the squares DO SOMETHING when clicked
   $('.square').on('click', function (event) {
-    alert('add symbol here')
+    // alert('add symbol here')
+    // this makes sure that we are selecting the right square so that we can
+    // and an X or an O to it
+    const currentSquare = $(this)
+    console.log(currentSquare)
+    // the logic will be to add a class to the square that has been clicked
+    // the class added will be an X or an O depending on the player
+    // Check if a box already has a class added (which means it has been clicked)
+    if (currentSquare.hasClass('letter-x') || currentSquare.hasClass('letter-o')) {
+      alert('BOOP! Square selected. Try another square')
+    } else {
+      // this is what happens when a square is empty
+      // player 1 goes and then switches to player 2
+      if (player === 1) {
+        currentSquare.addClass('letter-x')
+        player = 2
+      } else {
+        currentSquare.addClass('letter-o')
+        player = 1
+      }
+    }
   })
 })
